ddd520385f84caaacea5e654d964afe4eaa8ef77
pablomesny <pablomesny@gmail.com>
2023-03-05 22:23:33 -0300

    favorite model added

---
+++ models/favorite.js
@@ -0,0 +1,22 @@
+const { Schema, model } = require('mongoose');
+
+const FavoriteSchema = Schema({
+    properties: {
+        type: [Schema.Types.ObjectId],
+        ref: 'Property',
+        required: [ true, 'Property is mandatory' ]
+    },
+    user: {
+        type: Schema.Types.ObjectId,
+        ref: 'User',
+        required: [ true, 'User is mandatory' ]
+    }
+})
+
+FavoriteSchema.methods.toJSON = function() {
+    const { __v, _id, ...favorite } = this.toObject();
+    favorite.uid = _id;
+    return favorite;
+}
+
+module.exports = model( 'Favorite', FavoriteSchema );
